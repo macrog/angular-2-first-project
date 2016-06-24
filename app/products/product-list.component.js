27ba88822a6010382342b5cd31cb478eba005d7a
Maciek G <macrog>
2016-06-24 15:07:41 +0200

    adding product list ot table

--- app/products/product-list.component.js
+++ app/products/product-list.component.js
@@ -20,6 +20,29 @@ System.register(['angular2/core'], function(exports_1, context_1) {
         execute: function() {
             ProductListComponent = (function () {
                 function ProductListComponent() {
+                    this.panelTitle = 'Product List';
+                    this.products = [
+                        {
+                            "productId": 1,
+                            "productName": "Leaf Rake",
+                            "productCode": "GDN-0011",
+                            "releaseDate": "March 19, 2016",
+                            "description": "Leaf rake with 48-inch wooden handle.",
+                            "price": 19.95,
+                            "starRating": 3.2,
+                            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+                        },
+                        {
+                            "productId": 2,
+                            "productName": "Garden Cart",
+                            "productCode": "GDN-0023",
+                            "releaseDate": "March 18, 2016",
+                            "description": "15 gallon capacity rolling garden cart",
+                            "price": 32.99,
+                            "starRating": 4.2,
+                            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+                        }
+                    ];
                 }
                 ProductListComponent = __decorate([
                     core_1.Component({
